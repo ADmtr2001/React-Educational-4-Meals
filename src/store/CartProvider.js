c3bed7e9dc386671e4b0249eb7c50722ae233315
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-08 02:44:41 +0200

    add CartProvider component

---
+++ src/store/CartProvider.js
@@ -0,0 +1,15 @@
+import CartContext from './cart-context';
+
+const CartProvider = props => {
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler,
+    };
+
+    return <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>;
+}
